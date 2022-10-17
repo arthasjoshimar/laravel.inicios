a8ed70b372af95139ed324b07609d987b1512dcf
Jesus Callisaya <jesuscallisayasalinas@gmail.com>
2022-10-16 23:11:45 -0400

    manejo de rutas

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,22 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function() {
+Route::get('/', function () {
     return 'Home';
 });
 
-// Route::get('/usuarios/{id}', function($id) {
-//     return 'Obteneiendo id: '.$id;
-// })->where('id', '[0-9]+');
+Route::get('/usuarios/{id}', function ($id) {
+    return 'Obteneiendo id: ' . $id;
+})->where('id', '[0-9]+');
 
-// Route::get('/usuarios/nuevo', function() {
-//     return 'Crear nuevo usuario';
-// });
+Route::get('/usuarios/nuevo', function () {
+    return 'Crear nuevo usuario';
+});
 
-
+Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+    if ($nickname) {
+        return "Hola Amigos: $name con su nickname: $nickname";
+    } else {
+        return "Hola amigos $name, sin nickname";
+    }
+});
