7f0b6e9a2c9631b5c95758f7b0f569159631dde4
Jesus Callisaya <jesuscallisayasalinas@gmail.com>
2022-10-16 23:04:10 -0400

    Inicio proyecto

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function() {
+    return 'Home';
+});
+
+// Route::get('/usuarios/{id}', function($id) {
+//     return 'Obteneiendo id: '.$id;
+// })->where('id', '[0-9]+');
+
+// Route::get('/usuarios/nuevo', function() {
+//     return 'Crear nuevo usuario';
+// });
+
+
